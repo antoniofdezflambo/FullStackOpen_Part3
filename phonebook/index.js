0e24073c7d58a3b0825aee8af5725b950a3230f1
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-04-10 01:03:07 +0200

    Exercise 3.2 completed

--- phonebook/index.js
+++ phonebook/index.js
@@ -30,6 +30,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const now = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${now.toString()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
