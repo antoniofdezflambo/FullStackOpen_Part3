003db47e1f4c48374037e7b62534388202dc1f60
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-04-10 01:09:47 +0200

    Exercise 3.3 completed

--- phonebook/index.js
+++ phonebook/index.js
@@ -35,6 +35,14 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${now.toString()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if(person) response.json(person)
+  else response.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
