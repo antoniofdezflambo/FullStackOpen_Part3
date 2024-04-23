ea1629f5189a48aedb208a856529d52bef23be24
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-04-24 00:43:29 +0200

    Execise 3.15 completed

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -65,16 +65,15 @@ app.get('/info', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id).then(person => {
     response.json(person)
-  }).catch(error => {
-    response.status(204).end()
-  })
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -109,6 +108,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
